63b54df287738fe7c7104f0359a23222758723f7
nalee97 <mohamednaleef1564@gmail.com>
2023-03-15 22:17:38 +0530

    commit

--- src/IdentifierSplitter.java
+++ src/IdentifierSplitter.java
@@ -1,114 +1,45 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IdentifierSplitter {
-    public static void main(String[] args) {
-        String inputFile = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\Output.txt"; // replace with the actual input file path
-        String outputFile = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\splittedWords.txt"; // replace with the actual output file path
 
+    public void IdentifierSplitter()
+    {
+        String filePath = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\Output.txt";
+        String outputFilePath = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\split.txt";
         try {
-            ArrayList<String> identifiers = readIdentifiers(inputFile);
-            ArrayList<ArrayList<String>> splitIdentifiers = splitIdentifiers(identifiers);
-            writeOutput(splitIdentifiers, outputFile);
-            System.out.println("Successfully split identifiers and saved to " + outputFile);
-        } catch (FileNotFoundException e) {
-            System.out.println("Input file not found: " + inputFile);
-        } catch (IOException e) {
-            System.out.println("Error writing output file: " + outputFile);
-        }
-    }
-
-    public static ArrayList<String> readIdentifiers(String inputFile) throws FileNotFoundException {
-        ArrayList<String> identifiers = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(inputFile));
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine().trim();
-            if (!line.isEmpty()) {
-                identifiers.add(line);
+            FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(fr);
+            FileWriter fw = new FileWriter(outputFilePath);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = splitLine(line);
+                fw.write(Arrays.toString(words) + "\n");
             }
+            br.close();
+            fr.close();
+            fw.close();
+            System.out.println("Output written to file: " + outputFilePath);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
         }
-        scanner.close();
-        return identifiers;
     }
 
-    public static ArrayList<ArrayList<String>> splitIdentifiers(ArrayList<String> identifiers) {
-        ArrayList<ArrayList<String>> splitIdentifiers = new ArrayList<>();
-        for (String identifier : identifiers) {
-            ArrayList<String> parts = new ArrayList<>();
-            //parts.addAll(splitLine(identifier));
-            //parts.addAll(splitCamelCase(identifier));
-            //parts.addAll(splitUnderscore(identifier));
-            parts.addAll(splitAny(identifier));
-            splitIdentifiers.add(parts);
+    public static String[] splitLine(String line) {
+        Pattern pattern = Pattern.compile("[a-zA-Z_$][a-zA-Z_$0-9]*");
+        Matcher matcher = pattern.matcher(line);
+        String[] result = new String[0];
 
+        while (matcher.find()) {
+            String identifier = matcher.group();
+            String[] words = identifier.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+            result = Arrays.copyOf(result, result.length + words.length);
+            System.arraycopy(words, 0, result, result.length - words.length, words.length);
         }
-        return splitIdentifiers;
+        return result;
     }
-
-//    public static ArrayList<String> splitLine(String identifier) {
-//        ArrayList<String> parts = new ArrayList<>();
-//        Pattern pattern = Pattern.compile("[a-zA-Z_$][a-zA-Z_$0-9]*");
-//        Matcher matcher = pattern.matcher(identifier);
-//
-//        while (matcher.find()) {
-//            String part = matcher.group();
-//            String[] words = identifier.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
-//            System.out.println(Arrays.toString(words));
-//
-//        }
-//        return parts;
-//
-//
-//    }
-
-//    public static ArrayList<String> splitCamelCase(String identifier) {
-//        ArrayList<String> parts = new ArrayList<>();
-//        Pattern pattern = Pattern.compile("[a-z]+|[A-Z][a-z]*|[0-9]+");
-//        Matcher matcher = pattern.matcher(identifier);
-//        while (matcher.find()) {
-//            String part = matcher.group();
-//            if (!part.isEmpty()) {
-//                parts.add(part);
-//            }
-//        }
-//        return parts;
-//    }
-//
-//    public static ArrayList<String> splitUnderscore(String identifier) {
-//        ArrayList<String> parts = new ArrayList<>();
-//        String[] split = identifier.split("_");
-//        for (String part : split) {
-//            if (!part.isEmpty()) {
-//                parts.add(part);
-//            }
-//        }
-//        return parts;
-//    }
-//
-    public static ArrayList<String> splitAny(String identifier) {
-        ArrayList<String> parts = new ArrayList<>();
-        String[] split = identifier.split("[^a-zA-Z0-9]+");
-        for (String part : split) {
-            if (!part.isEmpty()) {
-                parts.add(part);
-            }
-        }
-        return parts;
-    }
-
-    public static void writeOutput(ArrayList<ArrayList<String>> splitIdentifiers, String outputFile) throws IOException {
-        FileWriter writer = new FileWriter(new File(outputFile));
-        for (ArrayList<String> parts : splitIdentifiers) {
-            writer.write(String.join(",", parts) + "\n");
-        }
-        writer.close();
-    }
-
 }
--- src/Research.java
+++ src/Research.java
@@ -28,7 +28,7 @@ public class Research {
         try (FileInputStream inputStream = new FileInputStream(file)) {
             inputStream.read(bytes);
             content = new String(bytes);
-            List<String> identifiers = extractIdentifiers(content);
+            List<String> identifiers = IdentifierExtractor.extractIdentifiers(content);
             try (PrintWriter writer = new PrintWriter("C:\\Users\\moham\\IdeaProjects\\final research\\src\\Output.txt")) {
                 for (String s : identifiers) {
                     writer.println(s);
@@ -37,14 +37,19 @@ public class Research {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            IdentifierSplitter();
+            //IdentifierSplitter();
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        IdentifierExtractor ex = new IdentifierExtractor();
+        ex.toString();
 
        // IdentifierSplitter is = new IdentifierSplitter();
+
+        IdentifierSplitter is = new IdentifierSplitter();
+        is.IdentifierSplitter();
 
         IdentifierChecker ir = new IdentifierChecker();
         ir.identfierchecker();
@@ -55,75 +60,73 @@ public class Research {
 
 
     }
-    public static List<String> extractIdentifiers(String sourceCode) {
-        final List<String> identifiers = new ArrayList<>();
+//    public static List<String> extractIdentifiers(String sourceCode) {
+//        final List<String> identifiers = new ArrayList<>();
+//
+//        // Create an AST parser
+//        ASTParser parser = ASTParser.newParser(AST.JLS10);
+//        parser.setSource(sourceCode.toCharArray());
+//        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+//
+//        // Parse the source code
+//        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+//
+//        // Visit each node in the AST and extract the identifiers
+//        cu.accept(new ASTVisitor() {
+//
+//            public boolean visit(TypeDeclaration declaration) {
+//                SimpleName newClass = declaration.getName();
+//                identifiers.add(newClass.getIdentifier().toString());
+//                return true;
+//            }
+//
+//            public boolean visit(MethodDeclaration declaration) {
+//                SimpleName newMethod = declaration.getName();
+//                identifiers.add(newMethod.getIdentifier().toString());
+//                return true;
+//            }
+//
+//            public boolean visit(VariableDeclarationFragment declaration1){
+//                SimpleName newVariable = declaration1.getName();
+//                identifiers.add(newVariable.getIdentifier().toString());
+//                return true;
+//            }
+//
+//        });
+//
+//        return identifiers;
+//    }
 
-        // Create an AST parser
-        ASTParser parser = ASTParser.newParser(AST.JLS10);
-        parser.setSource(sourceCode.toCharArray());
-        parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
-        // Parse the source code
-        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
-        // Visit each node in the AST and extract the identifiers
-        cu.accept(new ASTVisitor() {
 
-            public boolean visit(TypeDeclaration declaration) {
-                SimpleName newClass = declaration.getName();
-                identifiers.add(newClass.getIdentifier().toString());
-                return true;
-            }
+//    public static void IdentifierSplitter (){
+//            String filePath = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\Output.txt";
+//            try {
+//                FileReader fr = new FileReader(filePath);
+//                BufferedReader br = new BufferedReader(fr);
+//                String line;
+//                while ((line = br.readLine()) != null) {
+//                    splitLine(line);
+//                }
+//                br.close();
+//                fr.close();
+//            } catch (Exception e) {
+//                System.out.println("An error occurred while reading the file: " + e.getMessage());
+//            }
+//
+//    }
+//
+//        public static void splitLine(String line) {
+//            Pattern pattern = Pattern.compile("[a-zA-Z_$][a-zA-Z_$0-9]*");
+//            Matcher matcher = pattern.matcher(line);
+//
+//            while (matcher.find()) {
+//                String identifier = matcher.group();
+//                String[] words = identifier.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+//                System.out.println(Arrays.toString(words));
+//
+//        }
 
-            public boolean visit(MethodDeclaration declaration) {
-                SimpleName newMethod = declaration.getName();
-                identifiers.add(newMethod.getIdentifier().toString());
-                return true;
-            }
 
-            public boolean visit(VariableDeclarationFragment declaration1){
-                SimpleName newVariable = declaration1.getName();
-                identifiers.add(newVariable.getIdentifier().toString());
-                return true;
-            }
-
-        });
-
-        return identifiers;
     }
-
-
-
-    public static void IdentifierSplitter (){
-            String filePath = "C:\\Users\\moham\\IdeaProjects\\final research\\src\\Output.txt";
-            try {
-                FileReader fr = new FileReader(filePath);
-                BufferedReader br = new BufferedReader(fr);
-                String line;
-                while ((line = br.readLine()) != null) {
-                    splitLine(line);
-                }
-                br.close();
-                fr.close();
-            } catch (Exception e) {
-                System.out.println("An error occurred while reading the file: " + e.getMessage());
-            }
-
-    }
-
-        public static void splitLine(String line) {
-            Pattern pattern = Pattern.compile("[a-zA-Z_$][a-zA-Z_$0-9]*");
-            Matcher matcher = pattern.matcher(line);
-
-            while (matcher.find()) {
-                String identifier = matcher.group();
-                String[] words = identifier.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
-                System.out.println(Arrays.toString(words));
-
-        }
-
-
-    }
-
-
-}
